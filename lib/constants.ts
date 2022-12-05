a2e76664a0e7482c68f2ccc753e4b9c0ca1b2a9c
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-05 09:44:53 +0900

    add colors constants

---
+++ lib/constants.ts
@@ -0,0 +1,49 @@
+/* ---------------------------- Base theme colors --------------------------- */
+export const PRIMARY_COLOR = {
+  main: "#7C5DFA",
+  light: "#9277FF",
+};
+export const ERROR_COLOR = {
+  main: "#EC5757",
+  light: "#FF9797",
+};
+export const BACKGROUND_COLOR = {
+  light: {
+    paper: "#FFFFFF",
+    main: "#F8F8FB",
+  },
+  dark: {
+    paper: "#1E2139",
+    main: "#141625",
+  },
+};
+export const TEXT_COLOR = {
+  light: {
+    primary: "#0C0E16",
+    secondary: "#888EB0",
+  },
+  dark: {
+    primary: "#FFFFFF",
+    secondary: "#DFE3FA",
+  },
+};
+
+/* -------------------------- Invoice status colors ------------------------- */
+export const PAID_COLOR = {
+  main: "#33D69F",
+  background: "#33D69F90",
+};
+export const PENDING_COLOR = {
+  main: "#FF8F00",
+  background: "#FF8F0090",
+};
+export const DRAFT_COLOR = {
+  light: {
+    main: "#373B53",
+    background: "#373B5390",
+  },
+  dark: {
+    main: "#DFE3FA",
+    background: "#DFE3FA90",
+  },
+};
