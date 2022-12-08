5e9a1e30a1fa9da9d61262a2f45a4575fbd86d67
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-08 10:40:16 +0900

    filter invoices

--- components/Invoices/InvoicesList.tsx
+++ components/Invoices/InvoicesList.tsx
@@ -1,20 +1,27 @@
-import { Stack } from '@mui/material'
-import React from 'react'
-import { Invoice } from '../../lib/interfaces'
-import InvoiceCard from './InvoiceCard'
+import { Stack } from "@mui/material";
+import { useSelector } from "react-redux";
+import { Invoice } from "../../lib/interfaces";
+import { RootState } from "../../store/store";
+import InvoiceCard from "./InvoiceCard";
 
 type Props = {
-  invoices: Invoice[]
-}
+  invoices: Invoice[];
+};
 
-const InvoicesList = ({invoices}: Props) => {
+const InvoicesList = ({ invoices }: Props) => {
+  const filters = useSelector((state: RootState) => state.invoices.filters);
+
+  const filteredInvoices = invoices.filter((invoice) =>
+    filters.length > 0 ? filters.includes(invoice.status) : invoice
+  );
+
   return (
     <Stack gap={2}>
-      {invoices.map((invoice) => (
-        <InvoiceCard key={invoice._id.toString()} invoice={invoice}/>
+      {filteredInvoices.map((invoice: Invoice) => (
+        <InvoiceCard key={invoice._id.toString()} invoice={invoice} />
       ))}
     </Stack>
-  )
-}
+  );
+};
 
-export default InvoicesList
+export default InvoicesList;
