5f594d01e9dfe071ca2ab394f7b03f7364fae7fe
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-13 15:12:05 +0900

    refactor FormSection

--- components/CreateInvoice/FormSection.tsx
+++ components/CreateInvoice/FormSection.tsx
@@ -1,35 +1,27 @@
-import { Grid, SxProps, Theme, Typography } from '@mui/material';
+import { Grid, SxProps, Theme, Typography } from "@mui/material";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
   sectionHeading?: string;
-  sx?: SxProps<Theme> | ((theme: Theme) => SxProps)
+  sx?: SxProps<Theme> | ((theme: Theme) => SxProps);
 };
 
 const FormSection = ({ children, sectionHeading, sx }: Props) => {
   return (
-    <Grid
-      container
-      spacing={3}
-      sx={{
-        "& > div": { width: "100%" },
-        "& > div:nth-of-type(2)": sectionHeading
-          ? { paddingTop: "8px !important" }
-          : {},
-        "& > div:nth-of-type(1)": sectionHeading
-          ? {}
-          : { paddingTop: "8px !important" },
-        ...sx
-      }}
-    >
-      {sectionHeading && (
-        <Grid item xs={12}>
-          <SectionHeading>{sectionHeading}</SectionHeading>
-        </Grid>
-      )}
-      {children}
-    </Grid>
+    <div>
+      {sectionHeading && <SectionHeading>{sectionHeading}</SectionHeading>}
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          "& > div": { width: "100%" },
+          ...sx
+        }}
+      >
+        {children}
+      </Grid>
+    </div>
   );
 };
 
@@ -37,7 +29,7 @@ export default FormSection;
 
 const SectionHeading = ({ children }: Pick<Props, "children">) => {
   return (
-    <Typography color="primary" fontWeight="bold" variant="body1">
+    <Typography color="primary" fontWeight="bold" variant="body1" marginBottom={1.75}>
       {children}
     </Typography>
   );
